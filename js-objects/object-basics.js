344789ec20f7000360833119f0afab56aa407d75
Christian Tisby <cduane82@hotmail.com>
2019-07-13 17:55:35 -0400

    new commit

--- js-objects/object-basics.js
+++ js-objects/object-basics.js
@@ -5,4 +5,27 @@ let myBook = {
 
 }
 
-console.log(myBook)  
+console.log(`${myBook.title} by ${myBook.author}`)
+
+myBook.title = 'Animal Farm'
+
+console.log(`${myBook.title} by ${myBook.author}`)
+
+
+// Challenge Area
+
+// Name, age, location
+
+let person = {
+    name: 'Christian Tisby',
+    age: 37,
+    location: 'Tallahassee, Florida'
+}
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+person.age += 1
+
+console.log(`${person.name} is ${person.age} and lives in ${person.location}`)
+
+
